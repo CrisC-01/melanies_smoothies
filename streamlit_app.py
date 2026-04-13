53bd24bf5735e068ee8ffc77fbfd67834ca5244a
CrisC-01 <cristell.ceron01@gmail.com>
2026-04-13 10:10:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.write("The name on your Smoothie will be:",name_on_order)
 
 cnx = st.connection("snowflake")
 session=cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options")
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 #Convert the snowpark DataFarame to a Pandas dataFrame so we can use the LOC function
@@ -33,9 +33,13 @@ if ingredients_list:
         ingredients_string = ''
         for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen+' '
+
+            search_on=[d.df.loc[pd_df['FRUIT_NAME']==FRUIT_CHOSEN,['SEARCH_ON'].iloc[0]
+            st.write('The seacrh value for ',fruit_chosen,' is ',search_on, '.')
+                       
             st.subheader(fruit_chosen+'Nutrition Information')
-            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
-            sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+            fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
+            sf_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
         #st.write(ingredients_string)
 
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -50,5 +54,6 @@ if ingredients_list:
 
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/+fruit_chosen)
 sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
